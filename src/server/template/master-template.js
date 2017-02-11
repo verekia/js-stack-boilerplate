0d32a3df62aa54fa2411413846ddaf35d0a6e4b7
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-02-11 13:00:34 +0700

    Use cross-env instead of multiple webpack files

--- src/server/template/master-template.js
+++ src/server/template/master-template.js
@@ -12,7 +12,7 @@ export default (title: string) => `
   <body>
     <h1>${title}</h1>
     <div class="js-app"></div>
-    <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}`}/js/bundle.js"></script>
+    <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
   </body>
 </html>
 `
