7048b3feacdd23fe57dae96a40e1a8384d14ae9d
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-02-18 04:18:35 +0700

    Replace multiple PM2 envs par Nodemon, reorganization of package.json scripts

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -11,7 +11,6 @@ import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
-
 import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
 
