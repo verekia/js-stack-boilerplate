80d23af7b248d92479b138ef5ab3a89f1491d993
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-02-22 04:01:08 +0700

    Update release.sh for no-services

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 # This file is used to generate Github releases, please ignore it.
 
-name='js-stack-boilerplate-v2.4.0'
+name='js-stack-boilerplate-no-services-v2.4.0'
 dir='dist/'$name
 
 rm -rf dist
@@ -11,15 +11,10 @@ cp -R public $dir
 cp -R src $dir
 
 cp .babelrc $dir
-cp .coveralls.yml $dir
-cp .env-sample $dir
 cp .eslintrc.json $dir
 cp .flowconfig $dir
 cp .gitignore $dir
-cp .travis.yml $dir
-cp app.json $dir
 cp package.json $dir
-cp Procfile $dir
 cp webpack.config.babel.js $dir
 cp yarn.lock $dir
 
