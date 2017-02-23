01615e5584adddf88341485fcedfcf86c80bd15f
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-02-24 00:02:56 +0700

    Rewrite nav.jsx using a loop

--- src/shared/component/nav.jsx
+++ src/shared/component/nav.jsx
@@ -15,18 +15,18 @@ const Nav = () =>
     <Link to={HOME_PAGE_ROUTE} className="navbar-brand">{APP_NAME}</Link>
     <div className="js-navbar-collapse collapse navbar-collapse">
       <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <NavLink to={HOME_PAGE_ROUTE} className="js-nav-link nav-link" activeStyle={navLinkActiveStyle} exact>Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to={HELLO_PAGE_ROUTE} className="js-nav-link nav-link" activeStyle={navLinkActiveStyle}>Say Hello</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to={HELLO_ASYNC_PAGE_ROUTE} className="js-nav-link nav-link" activeStyle={navLinkActiveStyle}>Say Hello Asynchronously</NavLink>
-        </li>
-        <li className="nav-item">
-          <Link to="/404" className="js-nav-link nav-link">404</Link>
-        </li>
+        {
+          [
+            { route: HOME_PAGE_ROUTE, label: 'Home' },
+            { route: HELLO_PAGE_ROUTE, label: 'Say Hello' },
+            { route: HELLO_ASYNC_PAGE_ROUTE, label: 'Say Hello Asynchronously' },
+            { route: '/404', label: '404' },
+          ].map(link => (
+            <li className="nav-item" key={link.route}>
+              <NavLink to={link.route} className="js-nav-link nav-link" activeStyle={navLinkActiveStyle} exact>{link.label}</NavLink>
+            </li>
+          ))
+        }
       </ul>
     </div>
   </nav>
