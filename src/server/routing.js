05f78581e959f94c043537a0f771df7e65b50d49
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-02-23 23:17:46 +0700

    Add Fake Internal Server Error

--- src/server/routing.js
+++ src/server/routing.js
@@ -31,6 +31,10 @@ export default (app: Object) => {
     res.json(helloEndpoint(req.params.num))
   })
 
+  app.get('/500', () => {
+    throw Error('Fake Internal Server Error')
+  })
+
   app.get('*', (req, res) => {
     res.status(404).send(renderApp(req.url))
   })
