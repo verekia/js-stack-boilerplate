9d2d620f88133b867784e8fc879e157b5e75b573
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-02-25 18:12:23 +0700

    Use flow-disable-next-line

--- src/server/init-store.js
+++ src/server/init-store.js
@@ -1,3 +1,5 @@
+// @flow
+
 import * as Immutable from 'immutable'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
@@ -7,16 +9,14 @@ import helloReducer from '../shared/reducer/hello'
 const initStore = (plainPartialState: ?Object) => {
   const preloadedState = plainPartialState ? {} : undefined
 
-  if (plainPartialState) {
-    if (plainPartialState.hello) {
-      preloadedState.hello = helloReducer(undefined, {})
-        .merge(Immutable.fromJS(plainPartialState.hello))
-    }
+  if (plainPartialState && plainPartialState.hello) {
+    // flow-disable-next-line
+    preloadedState.hello = helloReducer(undefined, {})
+      .merge(Immutable.fromJS(plainPartialState.hello))
   }
 
-  return createStore(combineReducers({
-    hello: helloReducer,
-  }), preloadedState, applyMiddleware(thunkMiddleware))
+  return createStore(combineReducers({ hello: helloReducer }),
+    preloadedState, applyMiddleware(thunkMiddleware))
 }
 
 export default initStore
