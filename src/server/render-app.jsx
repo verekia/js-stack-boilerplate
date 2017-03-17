2b4a8c5736ee2a71f2911a1706c0b15cb8843270
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-03-17 11:24:57 +0800

    Add some meta tags

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -28,8 +28,10 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
     `<!doctype html>
     <html>
       <head>
+        <meta charset="utf-8">
         ${head.title}
         ${head.meta}
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="${STATIC_PATH}/css/bootstrap.min.css">
         <style class="${JSS_SSR_CLASS}">${sheets.toString()}</style>
       </head>
