d22fb68753501f8ef1453ce82fcf07662226a3d6
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-03-17 10:29:41 +0800

    Fix Promise rejection for save()

--- src/server/controller.js
+++ src/server/controller.js
@@ -9,7 +9,7 @@ export const homePage = () => null
 export const helloPage = () =>
   new Promise((resolve, reject) => {
     const helloMessage = new Message({
-      key: 'hello',
+      key: 'hello-msg',
       content: 'Server-side preloaded message from the DB',
     })
 
@@ -17,12 +17,12 @@ export const helloPage = () =>
     .then(
       /* eslint-disable no-console */
       () => console.log('Message collection dropped'),
-      () => console.log('Message collection not found'),
+      () => console.log('Message collection already empty'),
       /* eslint-enable no-console */
     )
-    .then(() => helloMessage.save(err => reject(err)))
+    .then(() => helloMessage.save((err) => { if (err) reject(err) }))
     .then(() => Message
-      .findOne({ key: 'hello' })
+      .findOne({ key: 'hello-msg' })
       .exec((err, result) => {
         if (err) reject(err)
         resolve({ hello: { message: result.content } })
