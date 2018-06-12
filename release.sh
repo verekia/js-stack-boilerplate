4ae289c707c9e29c991fed3521aeaf789b7f1539
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-06-12 14:40:13 +0200

    WIP - Reorganization, Bump packages

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 # This file is used to generate Github releases, please ignore it.
 
-version='v2.4.5'
+version='v3.0.0'
 name='js-stack-boilerplate-'$version
 dir='dist/'$name
 
@@ -11,17 +11,14 @@ mkdir $dir
 cp -R public $dir
 cp -R src $dir
 
-cp .babelrc $dir
+cp .dockerignore $dir
 cp .env-sample $dir
 cp .eslintrc.json $dir
 cp .flowconfig $dir
 cp .gitignore $dir
 cp .travis.yml $dir
-cp app.json $dir
 cp package.json $dir
 cp Procfile $dir
-cp webpack.config.babel.js $dir
-cp yarn.lock $dir
 
 cd dist
 zip -r -X $name'.zip' $name
